456250c06ddfe11cf4f07e006d8dfc6b74bf426f
Alex F. Bokov <bokov@uthscsa.edu>
2016-05-12 20:15:44 -0500

    Maybe need to commit updated tables?

--- chi2/chinotype.py
+++ chi2/chinotype.py
@@ -469,6 +469,7 @@ class Chi2:
 		update table {0} set c_description = c_name where c_description is null
 		'''.format(chischemes)
 		cols, rows = do_log_sql(db,sql)
+		db.commit()
 		sql = '''create index {0}_idx on {0} (c_name)'''.format(chischemes)
 		cols, rows = do_log_sql(db,sql)
 
