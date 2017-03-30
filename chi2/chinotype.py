c5afc645f3ec79040f59647b0ca858132c930137
Alex F. Bokov <bokov@uthscsa.edu>
2017-03-30 10:30:25 -0500

    No stopped calculating a dir column, tidied up a little

--- chi2/chinotype.py
+++ chi2/chinotype.py
@@ -776,11 +776,7 @@ class Chi2:
         )
         , data as (
             select prefix, ccd
-            , name
-            , {3}
-            , frc_{3} 
-            , {0}
-            , frc_{0}
+            , name, {3}, frc_{3}, {0}, frc_{0}
             -- , power({0} - (cohort.pat_count * frc_{3}), 2) / (cohort.pat_count * frc_{3}) chisq
             -- oops, that's not really chisq df=1, but the below is...
             , case 
@@ -797,7 +793,7 @@ class Chi2:
 	      else
 	      (1-frc_{3})*frc_{0}/((1-frc_{0})*frc_{3}) 
 	      end odds_ratio
-	    , case when frc_{3} = frc_{0} then 0 when frc_{3} < frc_{0} then 1 else -1 end dir
+	    --, case when frc_{3} = frc_{0} then 0 when frc_{3} < frc_{0} then 1 else -1 end dir
             from {1}
             , cohort
             where frc_{3} > 0   -- reference patient set frequency
